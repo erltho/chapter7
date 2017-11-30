48a613096959542959331e8864c2a6050dcd41eb
Erlend Thorsrud <erlendthorsrud>
2017-11-30 20:46:10 +0100

    Adding modules

---
+++ modules/dirlister.py
@@ -0,0 +1,9 @@
+import os
+def run(**args):
+    print "[*] In dirlister module."
+    files = os.listdir(".")
+    return str(filesimport os
+            def run(**args):
+            print "[*] In dirlister module."
+            files = os.listdir(".")
+            return str(files))
